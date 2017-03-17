631db7eab16a39f4c89715682acc075501dc82c7
vorwerkc <vorwerk@physik.hu-berlin.de>
2017-03-17 14:59:10 +0100

    Added docstring and removed tabs

--- excalibr/parseband.py
+++ excalibr/parseband.py
@@ -17,11 +17,11 @@ class bandstr:
     """
     def __init__(self,name):
         if os.path.isfile(name):
-            self.setparameter(name)
+            self._setparameter(name)
         else:
             print("\nScript needs xml file\"",name,"\"to run\n")
             sys.exit()
-    def setparameter(self,name):
+    def _setparameter(self,name):
         self.tree = etree.parse(name)
         self.filename=name
         if self.tree.xpath('/bandstructure/@character')[0]=='true':
@@ -35,17 +35,36 @@ class bandstr:
             self.pts = self.tree.xpath('/bandstructure/band[1]/point/@distance')
             self.bands =len(self.tree.xpath('/bandstructure/band'))
 
-    def getband(self, bandnr):
+    def _getband(self, bandnr):
         y = [float(xe)*27.211 for xe in self.tree.xpath("/bandstructure/band[%i]/point/@eval"%(bandnr))]
         return self.pts, y
 
     def getbands(self, bandmin, bandmax):
+        """
+        returns the energy bands for the path in reciprocal space when the band character of the
+        bands is not calculated. As often high-energy or core states are not of interest,
+        the function allows to specify a window for the bands.
+        Args:
+            param1 (int):: bandmin
+                lowest band to be included
+            param2 (int):: bandmax
+                highest band to be included
+        Returns:
+            list :: list of points along the high-symmetry path
+            list :: list of bands of size (bandmax-bandmin+1), where every item in the list
+                is a list of the energy values for every entry in x
+            float :: lowest energy value for lowest band in output
+            float :: highest energy value for highest band in output
+        Raises:
+            ValueError: Subroutine can not be used when the character of the bands is
+            included in the class. 
+        """
         if not self.character:
             y=[]
             for i in range(bandmin,bandmax):
                 if (i==bandmin):
-                    x=self.getband(i)[0]
-                y.append(self.getband(i)[1])
+                    x=self._getband(i)[0]
+                y.append(self._getband(i)[1])
             windowlow=min(y[0])
             windowhigh=max(y[-1])
         else:
@@ -53,16 +72,24 @@ class bandstr:
         return x,y, windowlow, windowhigh
 
     def getlabels(self):
+        """
+        Function to get the labels of high-symmetry points along the path in reciprocal
+        space as defined in bandstructure.xml
+        Returns:
+            float:: list of the position for the labels along the x-axis
+            str :: list that contains the label for each point along the axis. For the GAMMA point,
+                the label is written as $\Gamma to allow the direct usage in matplotlib
+        """
         nrlabel=len(self.tree.xpath('/bandstructure/vertex'))
         x=[]
-        y=[]
+        label=[]
         for i in range(0,nrlabel):
             x.append(float(self.tree.xpath('/bandstructure/vertex[%i]/@distance'%(i+1))[0]))
-            y.append(str(self.tree.xpath('/bandstructure/vertex[%i]/@label'%(i+1))[0]))
-            if (y[i]=='GAMMA'): y[i]='$\Gamma$'
-        return x,y
+            label.append(str(self.tree.xpath('/bandstructure/vertex[%i]/@label'%(i+1))[0]))
+            if (label[i]=='GAMMA'): label[i]='$\Gamma$'
+        return x,label
 
-    def get_character_bands(self):
+    def _get_character_bands(self):
         if self.character:
             out=[]
             bands=self.tree.xpath('/bandstructure/species[1]/atom[1]/band')
@@ -73,7 +100,7 @@ class bandstr:
             raise ValueError('Subroutine not defined if character is false!')
         return self.pts, out
 
-    def get_bandcharacter(self,species,atom,l):
+    def _get_bandcharacter(self,species,atom,l):
         if self.character:
             out_bands=[]
             bands=self.tree.xpath('/bandstructure/species[@speciesnr="%s"]/atom[@atomnr="%s"]/band'%(species, atom))
@@ -85,8 +112,31 @@ class bandstr:
         return out_bands
 
     def atoml(self,species, atom, l):
-        pts, bands=self.get_character_bands()
-        characters=self.get_bandcharacter(species,atom, l)
+        """
+        Function to get the bandstructure and the character of the bands, where the
+        projection is on a specific atom and l-channel. Note that the output of this function is 
+        different from that of getbands, to be consistent with 
+        matplotlib.pyplot.scatter.
+        Args:
+            int :: species
+                species number as defined in the exciting input file. Note that in this
+                definition, the first species has speciesnumber=1
+            int :: atom
+                atom number of the species as defined in the exciting input file. Note 
+                that the first atom of each species has atomnumber=1
+            int :: l
+                l-channel to be projected on. Only projection on l=0, l=1, l=2, or 
+                l=3 has been implemented in exciting yet.
+
+        Returns:
+            list :: energy points for each band in one list of size 
+                len(self.pts)*self.bands
+            list :: energy value for each entry in the first output
+            list :: character for each entry in the first output
+        """
+       
+        pts, bands=self._get_character_bands()
+        characters=self._get_bandcharacter(species,atom, l)
         pts_out=[]
         for i in range(self.bands):
             for j in range(len(self.pts)):
@@ -94,7 +144,25 @@ class bandstr:
         return pts_out, bands, characters
     
     def atom(self, species, atom):
-        pts,bands=self.get_character_bands()
+        """
+        Function to get the bandstructure and the character of the bands, where the
+        projection is on a specific atom. Note that the output of this function is 
+        different from that of getbands, to be consistent with 
+        matplotlib.pyplot.scatter.
+        Args:
+            int :: species
+                species number as defined in the exciting input file. Note that in this
+                definition, the first species has speciesnumber=1
+            int :: atom
+                atom number of the species as defined in the exciting input file. Note 
+                that the first atom of each species has atomnumber=1
+        Returns:
+            list :: energy points for each band in one list of size 
+                len(self.pts)*self.bands
+            list :: energy value for each entry in the first output
+            list :: character for each entry in the first output
+        """
+        pts,bands=self._get_character_bands()
         out_bands=[]
 
         pts_out=[]
@@ -106,5 +174,82 @@ class bandstr:
             for pts in band.xpath('./point'):
                 out_bands.append(pts.xpath('./@sum'))
         return pts_out, bands, out_bands  
-            
+
+    def speciesl(self, species, l):
+        """
+        Function to get the bandstructure and the character of the bands, where the
+        projection is on a specific species and l-channel. Note that the output of this function is 
+        different from that of getbands, to be consistent with 
+        matplotlib.pyplot.scatter.
+        Args:
+            int :: species
+                species number as defined in the exciting input file. Note that in this
+                definition, the first species has speciesnumber=1
+            int :: l
+                l-channel to be projected on. Only projection on l=0, l=1, l=2, or 
+                l=3 has been implemented in exciting yet.
+        Returns:
+            list :: energy points for each band in one list of size 
+                len(self.pts)*self.bands
+            list :: energy value for each entry in the first output
+            list :: character for each entry in the first output
+        """
+
+        pts, bands=self._get_character_bands()
+
+        pts_out=[]
+        for i in range(self.bands):
+            for j in range(len(self.pts)):
+                pts_out.append(pts[j])
+
+        atoms=self.tree.xpath('/bandstructure/species[@speciesnr="%s"]'%species)
+        i=0
+        for atom in atoms:
+            if i==0:
+                character=np.array(self.atoml(species,i+1,l)[2])
+                i+=1
+            else:
+                character=character+np.array(self.atoml(species,i+1,l)[2])
+                i+=1
+
+        return pts_out, bands, character.tolist()
+
+    def species(self, species):
+        """
+        Function to get the bandstructure and the character of the bands, where the
+        projection is on a specific species. Note that the output of this function is 
+        different from that of getbands, to be consistent with 
+        matplotlib.pyplot.scatter.
+        Args:
+            int :: species
+                species number as defined in the exciting input file. Note that in this
+                definition, the first species has speciesnumber=1
+        Returns:
+            list :: energy points for each band in one list of size 
+                len(self.pts)*self.bands
+            list :: energy value for each entry in the first output
+            list :: character for each entry in the first output
+        """
+       
+        pts, bands=self._get_character_bands()
+
+        pts_out=[]
+        for i in range(self.bands):
+            for j in range(len(self.pts)):
+                pts_out.append(pts[j])
+
+        atoms=self.tree.xpath('/bandstructure/species[@speciesnr="%s"]'%species)
+        i=0
+        for atom in atoms:
+            if i==0:
+                character=np.array(self.atom(species,i+1)[2])
+                i+=1
+            else:
+                character=character+np.array(self.atoml(species,i+1)[2])
+                i+=1
+
+        return pts_out, bands, character.tolist()
+
+
+
 
--- excalibr/parsedos.py
+++ excalibr/parsedos.py
@@ -7,84 +7,161 @@ from math import *
 import os
 
 class dos:
-	"""
-	Object to store the data contained in the dos.xml file
-	of an exciting calculation.
-	Args:
-		name (string): filename of the dos xml information
-	.. attribute :: name
-	 	associated filename
+    """
+    Object to store the data contained in the dos.xml file
+    of an exciting calculation.
+    Args:
+        name (string): filename of the dos xml information
+    .. attribute :: name
+        associated filename
 
-	"""
-	def __init__(self, name):
-		if os.path.isfile(name):
-			self.setparameter(name)
-		else:
-			print ("\nScript needs xml file\"",name,"\"to run\n")
-			sys.exit()
-	def setparameter(self,name):
-			self.tree = etree.parse(name)
-			self.filename=name
-			self.natoms=len(self.tree.xpath('/dos/partialdos'))
-			xa = self.tree.xpath('/dos/totaldos/diagram[@nspin="1"]/point/@e')
-			if len(self.tree.xpath('/dos/totaldos/diagram'))==2:
-				self.spin=True
-			else:
-				self.spin=False
-			self.energies = [float(xe)*27.21138 for xe in xa]
-			self.nw=len(self.energies)
-	def getdos(self,array):
-		if not self.spin:
-			pdos=np.zeros(self.nw)
-			for m in array:
-				mdos = [float(xe) for xe in m.xpath("./point/@dos")]
-				for i in range(0,self.nw):
-					pdos[i] = pdos[i] + mdos[i]
-		elif self.spin:
+    """
+    def __init__(self, name):
+        if os.path.isfile(name):
+            self._setparameter(name)
+        else:
+            print ("\nScript needs xml file\"",name,"\"to run\n")
+            sys.exit()
+    def _setparameter(self,name):
+        self.tree = etree.parse(name)
+        self.filename=name
+        self.natoms=len(self.tree.xpath('/dos/partialdos'))
+        xa = self.tree.xpath('/dos/totaldos/diagram[@nspin="1"]/point/@e')
+        if len(self.tree.xpath('/dos/totaldos/diagram'))==2:
+            self.spin=True
+        else:
+            self.spin=False
+        self.energies = [float(xe)*27.21138 for xe in xa]
+        self.nw=len(self.energies)
+    def _getdos(self,array):
+        if not self.spin:
+            pdos=np.zeros(self.nw)
+            for m in array:
+                mdos = [float(xe) for xe in m.xpath("./point/@dos")]
+                for i in range(0,self.nw):
+                    pdos[i] = pdos[i] + mdos[i]
 
-			uppdos=np.zeros(self.nw)
-			downpdos=np.zeros(self.nw)
-			for m in array:
-				if m.get("nspin")=='1':
-					mdos = [float(xe) for xe in m.xpath("./point/@dos")]
-					for i in range(0,self.nw):
-						uppdos[i] = uppdos[i] + mdos[i]
-				elif m.get("nspin")=='2':
-					mdos = [float(xe) for xe in m.xpath("./point/@dos")]
-					for i in range(0,self.nw):
-						downpdos[i] = downpdos[i] + mdos[i]
-			pdos=[uppdos, downpdos]
-		return  pdos
+        elif self.spin:
+            uppdos=np.zeros(self.nw)
+            downpdos=np.zeros(self.nw)
+            for m in array:
+                if m.get("nspin")=='1':
+                    mdos = [float(xe) for xe in m.xpath("./point/@dos")]
+                    for i in range(0,self.nw):
+                        uppdos[i] = uppdos[i] + mdos[i]
+                elif m.get("nspin")=='2':
+                    mdos = [float(xe) for xe in m.xpath("./point/@dos")]
+                    for i in range(0,self.nw):
+                        downpdos[i] = downpdos[i] + mdos[i]
+                    pdos=[uppdos, downpdos]
+        return  pdos
 
-	def species(self, species):
-		m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"]/diagram'%(species))
-		pdos=self.getdos(m_array)
-		return pdos
+    def species(self, species):
+            """
+            Function to obtain the DOS projected onto a specific species
+            Args:
+                int:: species
+                    species number onto which to project. The speciesnumber is defined
+                    in the input.xml. Note that speciesnumber=1 for the first species.
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
+        m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"]/diagram'%(species))
+        pdos=self._getdos(m_array)
+        return pdos
 
-	def atom(self, species, atom):
-		m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"][@atom="%s"]/diagram'%(species,atom))
-		pdos=self.getdos(m_array)
-		return pdos
+    def atom(self, species, atom):
+            """
+            Function to obtain the DOS projected onto a specific atom
+            Args:
+                int:: species
+                    species number onto which to project. The number for each species
+                    can be found in INFO.OUT. Note that speciesnumber=1 for the first 
+                    species.
+                int :: atom
+                    atom number onto which to project. The number can be found in INFO.OUT.
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
+        m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"][@atom="%s"]/diagram'%(species,atom))
+        pdos=self._getdos(m_array)
+        return pdos
 
-	def angular(self,l):
-		m_array =self.tree.xpath('/dos/partialdos/diagram[@l="%s"]'%(l))
-		pdos=self.getdos(m_array)
-		return pdos
+    def angular(self,l):
+            """
+            Function to obtain the DOS projected onto a specific l-channel
+            Args:
+                int:: l
+                l-channel of the projection
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
 
-	def speciesl(self,species,l):
-		m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"]/diagram[@l="%s"]'%(species,l))
-		pdos=self.getdos(m_array)
-		return pdos
+            m_array =self.tree.xpath('/dos/partialdos/diagram[@l="%s"]'%(l))
+        pdos=self._getdos(m_array)
+        return pdos
 
-	def atoml(self, species,atom,l):
-		m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"][@atom="%s"]/diagram[@l="%s"]'%(species,atom,l))
-		pdos=self.getdos(m_array)
-		return pdos
-	def interstitial(self):
-		m_array =self.tree.xpath('/dos/interstitialdos/diagram')
-		pdos=self.getdos(m_array)
-		return pdos
-	def total(self):
-		m_array=self.tree.xpath('/dos/totaldos/diagram')
-		pdos=self.getdos(m_array)
-		return pdos
+    def speciesl(self,species,l):
+            """
+            Function to obtain the DOS projected onto a specific species and l-channel
+            Args:
+                int:: species
+                    species number onto which to project. The number for each species
+                    can be found in INFO.OUT. Note that speciesnumber=1 for the first 
+                    species.
+                int :: l
+                    l-channel of the projection
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
+
+        m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"]/diagram[@l="%s"]'%(species,l))
+        pdos=self._getdos(m_array)
+        return pdos
+
+    def atoml(self, species,atom,l):
+            """
+            Function to obtain the DOS projected onto a specific atom and l-channel
+            Args:
+                int:: species
+                    species number onto which to project. The number for each species
+                    can be found in INFO.OUT. Note that speciesnumber=1 for the first 
+                    species.
+                int :: atom
+                    atom number onto which to project. The number can be found in INFO.OUT.
+                int :: l
+                    l-channel of the projection
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
+
+        m_array =self.tree.xpath('/dos/partialdos[@speciesrn="%s"][@atom="%s"]/diagram[@l="%s"]'%(species,atom,l))
+        pdos=self._getdos(m_array)
+        return pdos
+    def interstitial(self):
+            """
+            Function to obtain the DOS projected onto the interstitial states
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
+
+        m_array =self.tree.xpath('/dos/interstitialdos/diagram')
+        pdos=self._getdos(m_array)
+        return pdos
+    def total(self):
+            """
+            Function to obtain total DOS
+            Returns:
+                list :: list with DOS value in $Ha^{-1}\Omega^{-1}$, where $\Omega$ is 
+                    the volume of the unit cell. The list has length len(dos.energies)
+            """
+
+            m_array=self.tree.xpath('/dos/totaldos/diagram')
+        pdos=self._getdos(m_array)
+        return pdos
